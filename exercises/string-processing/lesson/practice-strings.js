9f0be60cfa716f348d34c91c988ea1525c2137b8
Miles Abbason <miles.abbason@gmail.com>
2021-12-11 15:12:14 -0500

    Finished up through strings 1.3

--- exercises/string-processing/lesson/practice-strings.js
+++ exercises/string-processing/lesson/practice-strings.js
@@ -1,4 +1,5 @@
 "use strict";
+const readline = require('readline-sync');
 
 let [ firstName, lastName ] = [ 'Miles', 'Abbason' ];
 
@@ -29,5 +30,30 @@ let [ aIndex, vIndex ] = [ language.indexOf('a'), language.indexOf('v') ];
 // console.log(language.substr(aIndex, 4));
 // console.log(language.substr(vIndex, 4));
 
-console.log(language.substring(aIndex, 4));
-console.log(language.substring(vIndex, 4));
+// console.log(language.substring(aIndex, 4));
+// console.log(language.substring(vIndex, 4));
+
+let [ fact1, fact2 ] = ['Javascript is fun', 'Kids like it too' ];
+let compoundSentence = fact1.concat(' and ', fact2.toLowerCase());
+// console.log(compoundSentence);
+
+// console.log(fact1[0], fact2[0]);
+
+let pi = (22 / 7);
+// console.log(pi.toString().lastIndexOf('14'));
+
+let boxNumber = (356).toString();
+// console.log(boxNumber);
+
+// boxNumber = parseInt(boxNumber, 10);
+// console.log(typeof boxNumber);
+// boxNumber = String(boxNumber);
+// console.log(typeof boxNumber);
+
+let name = readline.question('What is your name? ');
+
+if (name.endsWith('!')) {
+  console.log(`HELLO ${name.slice(0, name.length - 1).toUpperCase()}. WHY ARE WE SCREAMING?`);
+} else {
+  console.log(`Hello ${name}.`)
+}
